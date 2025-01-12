9d2543baffae28da83c31e39ff8bdeeb2ba57084
sscc20111 <mw92559404@gmail.com>
2025-01-13 03:50:09 +0900

    reference1

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a><h2>LOGO</h2></a>
+      </header>
+      <main>
+        <section className='section section01'>
+          <div>
+            {/* <div className='imgBox'></div> */}
+            <div className='textBox'>
+              <p>Tempor cupidatat enim deserunt minim exercitation laborum non.</p>
+              <h4>test</h4>
+            </div>
+          </div>
+        </section>
+        <section className='section section02'>
+          <div>
+            <div className='leftBox'>
+              <span>test</span>
+              <p>Do deserunt aliqua laborum in ipsum exercitation in labore.</p>
+            </div>
+            <div className='rightBox'>
+              <div>
+                <ul>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                  <li>
+                    <div className='leftBox'>
+                      <p>Nisi do occaecat</p>
+                      <span>Tempor nulla nulla duis irure occaecat.</span>
+                    </div>
+                    <div className='rightBox'>
+                      <p>1424</p>
+                      <span>KRW Billion</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className='section section03'></section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
