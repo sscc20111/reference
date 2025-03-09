1640f9d0d34f965387acb1fdeee447622bec6aaa
aggie0123 <https://dlckdrb6176@gmail.com>
2025-03-09 23:02:09 +0900

    DetailPage_ing

--- src/page/detail.js
+++ src/page/detail.js
@@ -1,12 +1,41 @@
 import Header from "../assets/components/header";
+import year from "../assets/image/year.png"
 
 const Detail = () => {
 
     return(<>
     <Header></Header>
     <div className="DetailWrap">
+        <div className="intro">
+            <div>sello</div>
+            <h2>2주년 이벤트</h2>
+            <hr />
+        </div>
         <div className="detail">
-            detail Page
+            <div className="leftbox">
+                <img src={year}></img>
+            </div>
+            <div className="rightbox">
+                <ul>
+                    <li>셀로 2주년을 기념하여
+                        특별한 이벤트 참여를 유도하기 위해
+                        생일파티에 초대받는 컨셉의 메인 그래픽
+                        디자인을 작업하였다.
+                    </li>
+                    <li>
+                        <a>WORK SCOPE</a>
+                        <a>상세페이지 디자인</a>
+                    </li>
+                    <li>
+                        <a>Tool</a>
+                        <a>Photoshop</a>
+                        <a>Illustration</a>
+                    </li>
+                    <li className="link">
+                        <a href="https://sell-o.kr/events/2anniversary">https://sell-o.kr/events/2anniversary</a>    
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     </>)
--- src/page/sub.js
+++ src/page/sub.js
@@ -1,9 +1,27 @@
 import Header from "../assets/components/header";
 import design from "../assets/image/design.png"
 import year from "../assets/image/2year.png"
+import epilogue from "../assets/image/epilogue.png"
+import recomment from "../assets/image/recomment.png"
+import iphone from "../assets/image/iphone16.png"
+import z6 from "../assets/image/z6.png"
+import s24FE from "../assets/image/s24fe.png"
+import sc from "../assets/image/sc.png"
+import ai from "../assets/image/ai.png"
+import box from "../assets/image/boxing.png"
+import subtract from "../assets/image/subtract.png"
+import booqoo from "../assets/image/booqoo.png"
+import toss01 from "../assets/image/toss01.png"
+import toss02 from "../assets/image/toss02.png"
+import retro from "../assets/image/retro.png"
+import clean from "../assets/image/clean.png"
 import thumnail_z5 from "../assets/image/thumnail_z5.png"
-import uiux from "../assets/image/uiux.png"
+import thumnail_dog from "../assets/image/thumnail_dog.png"
+import price from "../assets/image/z5_price.png"
 import insta from "../assets/image/insta_01.png"
+import uiux from "../assets/image/uiux.png"
+
+
 
 const Sub = () => {
 
@@ -22,52 +40,72 @@ const Sub = () => {
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={epilogue}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={recomment}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={iphone}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={z6}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={s24FE}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={sc}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={ai}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={box}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={subtract}></img>
                             </a>
                         </li>
                         <li>
                             <a href="#detail">
-                                <img src={design}></img>
+                                <img src={booqoo}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#detail">
+                                <img src={toss01}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#detail">
+                                <img src={toss02}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#detail">
+                                <img src={retro}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#detail">
+                                <img src={clean}></img>
                             </a>
                         </li>
                     </ul>
@@ -84,52 +122,17 @@ const Sub = () => {
                         </li>
                         <li>
                             <a href="#detail">
+                                <img src={thumnail_dog}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#detail">
+                                <img src={price}></img>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#detail">
                                 <img src={insta}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#detail">
-                                <img src={design}></img>
                             </a>
                         </li>
                     </ul>
